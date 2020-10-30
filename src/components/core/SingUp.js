c5416f09cb7ceb7dc31222d81fae373a52ca43c6
UtpalMaji <utpalmaji.um@gmail.com>
2020-10-30 07:21:45 +0530

    add: firstName & lastName field at singup component

--- src/components/core/SingUp.js
+++ src/components/core/SingUp.js
@@ -27,13 +27,37 @@ class SingUp extends Component {
       <div className="App">
         <Grid container direction='row' wrap='nowrap' spacing={2} className='main-container'>
           <Grid container item md={9} sm={12} spacing={2} className='main-module' >
-          <Grid item md={12} sm={12}>
-          <Header  />
-          </Grid>
-            
+            <Grid item md={12} sm={12}>
+              <Header />
+            </Grid>
+            <Grid item md={6} sm={12} xs={12} className='input-field'>
+              <TextField
+                fullWidth
+                required={true}
+                label="First Name"
+                helperText={this.state.firstNameHelperText}
+                margin="dense"
+                variant="outlined"
+                error={this.state.fiestNameFlag === true ? false : true}
+                onChange={this.checkFirstName}
+              />
+            </Grid>
+            <Grid item md={6} sm={12} xs={12} py={4} className='input-field'>
+              <TextField
+                fullWidth
+                required={true}
+                label="Last Name"
+                helperText={this.state.lastNameHelperText}
+                margin="dense"
+                variant="outlined"
+                error={this.state.lastNameFlag === true ? false : true}
+                onChange={this.checkSecondName}
+              />
+            </Grid>
+
           </Grid>
           <Grid container alignContent='center' item md={3} className='secondery-module'>
-           
+
           </Grid>
         </Grid>
       </div>
